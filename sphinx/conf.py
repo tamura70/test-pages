6c17c8ddcf4dd813d6824e2444f6adf889b70d4a
Naoyuki Tamura <tamura@kobe-u.ac.jp>
2025-05-19 12:31:00 +0900

    modified:   sphinx/_build/doctrees/environment.pickle 	renamed:    .nojekyll -> sphinx/_build/html/.nojekyll 	modified:   sphinx/conf.py

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -31,7 +31,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages'
 ]
 
 autodoc_default_flags = [
